d4dedc2e2c380ff55d420130a7056007ce500a96
Enzhaev Ivan <8observer8@gmail.com>
2014-09-11 10:41:48 +0400

    First Commit

--- Dialog.cpp
+++ Dialog.cpp
@@ -19,13 +19,16 @@ void Dialog::on_resultButton_clicked()
 {
     bool ok;
 
+    // Input
     int a = ui->aLineEdit->text().toInt( &ok );
     if ( !ok ) {
         QMessageBox::information( this, tr( "Ошибка" ), tr( "Вы ввели некорректные данные" ) );
         return;
     }
 
+    // Result
     int b = ( a / 100 ) * 10 + a % 10;
 
+    // Output
     ui->bLineEdit->setText( QString::number( b ) );
 }
